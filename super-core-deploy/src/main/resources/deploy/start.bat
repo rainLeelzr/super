c5180f8d39ebeb99d298494eaf2549f477b6745d
rain <lizhirong100@163.com>
2021-02-24 16:06:03 +0800

    [optimize] 支持打包产物在windows运行时的控制台中文输出

--- super-core-deploy/src/main/resources/deploy/start.bat
+++ super-core-deploy/src/main/resources/deploy/start.bat
@@ -1,3 +1,4 @@
 title "@project.artifactId@"
-java -Xms512M -Xmx2048M -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=2048M -jar @project.artifactId@.jar
+chcp 65001
+java -Xms512M -Xmx2048M -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=2048M -Dfile.encoding=UTF-8 -jar @project.artifactId@.jar
 pause
